ffc3d2588756df18934f384d68da9685b9f2c35b
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-10-29 14:10:49 +0900

    emailの誤ったバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,    unique: true
   validates :nickname, presence: true
   validates :birthday, presence: true
 

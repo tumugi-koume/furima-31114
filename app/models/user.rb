da1c033d599c837b36f5dde8bdc151e15d10ec48
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-10-30 14:46:18 +0900

    presence: trueの重複をwith_optionsでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :birthday, presence: true
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX

9178dc0996915261987f6e045cf56bfbc2bef963
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-10-30 19:52:39 +0900

    statusモデルの作成とアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+
+  with_options presence :true do
+    validates :name
+    validates :text
+    validates :category
+    validates :status
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+  end
+
 end

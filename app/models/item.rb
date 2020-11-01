1df047e379e6090ff5fa5f289be9078327088141
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-01 15:46:28 +0900

    area,ship_dateモデルの作成、アソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :area
+  belongs_to_active_hash :ship_date
 
   with_options presence :true do
     validates :name
@@ -13,12 +15,16 @@ class Item < ApplicationRecord
     validates :category
     validates :status
     validates :delivery_charge
+    validates :area
+    validates :ship_date
   end
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :delivery_charge_id
+    validates :area_id
+    validates :ship_date_id
   end
 
 end

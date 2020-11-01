39b52109d489da7af53e9ec08d551ea7f76eeffa
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-01 15:32:49 +0900

    delivery_chargeモデル作成とアソシエーション、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,17 +5,20 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
 
   with_options presence :true do
     validates :name
     validates :text
     validates :category
     validates :status
+    validates :delivery_charge
   end
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
+    validates :delivery_charge_id
   end
 
 end

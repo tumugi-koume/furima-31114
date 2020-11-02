47c87299ae60185051269e07352d68ed25405bc0
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-02 15:49:19 +0900

    FactoryBot訂正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :item do
     name               {Faker::String.romdom(length: 4)}
-    text               {Faker::String.romdom(length: 3..20)}
+    text               {Faker::Lorem.sentence}
     category_id        {Faker::Number.between(from: 2, to: 11)}
     status_id          {Faker::Number.between(from: 2, to: 7)}
     delivery_charge_id {Faker::Number.between(from: 2, to: 3)}
     area_id            {Faker::Number.between(from: 2, to: 48)}
     ship_date_id       {Faker::Number.between(from: 2, to: 4)}
     price              {Faker::Number.between(from: 300, to: 9999999)}
+    image              {Faker::Lorem.sentence}
     association :user
   end
 end

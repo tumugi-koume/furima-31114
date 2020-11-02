8f8b378e5c293dc1cf3ed31f28e18512c2fef849
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-02 10:49:56 +0900

    priceのバリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,4 +29,9 @@ class Item < ApplicationRecord
     validates :ship_date_id
   end
 
+  validates :price, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9999999
+   }
 end

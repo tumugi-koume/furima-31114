74dde94dbd0204cc5a422e858435f2c3d8bce157
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-03 19:23:38 +0900

    Item.find(params[:id])をbefore_actionでまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
+  
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,8 +41,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :category_id, :status_id, :delivery_charge_id, :area_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def item_find
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     redirect_to action: :index unless current_user.id == @item.user_id
   end
 end

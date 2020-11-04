15c22910eecc699ff202cece54e8d1b5229c59d4
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-04 10:58:56 +0900

    rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy, :move_to_index]
   before_action :move_to_index, only: [:edit, :destroy]
-  
 
   def index
     @items = Item.order('created_at DESC')
@@ -36,11 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.destroy
   end
-  
+
   private
 
   def item_params

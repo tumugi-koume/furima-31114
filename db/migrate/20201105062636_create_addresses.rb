466a37b7825488345dd149df5429a69ea1e5373d
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-05 16:17:17 +0900

    order,addressテーブルのカラム設定

--- db/migrate/20201105062636_create_addresses.rb
+++ db/migrate/20201105062636_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :postal_code, null: false
+      t.integer    :area_id,     null: false
+      t.string     :city,        null: false
+      t.string     :street,      null: false
+      t.string     :building
+      t.string     :phone,       null: false
+      t.references :order,       null: false, foreign_key: true
       t.timestamps
     end
   end

fb4f3c52b67686fcc1fee72c38f13964f6170e4f
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-05 15:54:54 +0900

    index,createアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+
+  private
+  def order_params
+    item = Item.find(params[:id])
+    params.require(:order_address).permit(:postal_code, :area, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: item.id)
+  end
+
 end

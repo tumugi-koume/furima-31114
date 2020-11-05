f18c204a0d23b2013a527b37eccd374aba077db9
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-05 19:06:08 +0900

    form_withにorders#createを設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,20 @@ class OrdersController < ApplicationController
   end
 
   def create
+    binding.pry
+    # @item = Item.find(params[:id])
     @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
-    else
-      render action: :index
-    end
+    # if @order_address.valid?
+    #   @order_address.save
+    #   redirect_to root_path
+    # else
+    #   render action: :index
+    # end
+  end
 
   private
   def order_params
-    item = Item.find(params[:id])
+    # item = Item.find(params[:id])
     params.require(:order_address).permit(:postal_code, :area, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: item.id)
   end
 

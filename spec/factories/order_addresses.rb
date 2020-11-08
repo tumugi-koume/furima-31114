b84d2e428a398308515517f405d76b2dc187d3f3
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-08 12:56:05 +0900

    テストコード用ファイル作成と、exampleを記述

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567'}
-    area_id { 17 }
-    city { '中野区' }
-    street { '1-1' }
-    phone { '09012345678' }
-    user_id { 1 }
-    item_id { 1 }
-    association :user
-    association :item
+    area_id     { 17 }
+    city        { '中野区' }
+    street      { '1-1' }
+    phone       { '09012345678' }
+    user_id     { 1 }
+    item_id     { 1 }
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,54 @@ RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
       @order_address = FactoryBot.build(:order_address)
-  end
+    end
 
-  it '全ての値が正しく入力されていれば保存できること'
+    it '全ての値が正しく入力されていれば保存できること' do
+    end
+
+    it 'postal_codeが空だと保存できないこと' do
+    end
+
+    it 'area_idが空だと保存できないこと' do
+    end
+
+    it 'cityが空だと保存できないこと' do
+    end
+
+    it 'streetが空だと保存できないこと' do
+    end
+
+    it 'phoneが空だと保存できないこと' do
+    end
+
+    it 'user_idが空だと保存できないこと' do
+    end
+
+    it 'item_idが空だと保存できないこと' do
+    end
+
+    it 'postal_codeに-（ハイフン）が入っていないと保存されないこと' do
+    end
+
+    it 'postal_codeが数字でないと保存されないこと' do
+    end
+
+    it 'postal_codeが「3桁の数字-（ハイフン）4桁の数字」の形でないと保存できないこと' do
+    end
+
+    it 'area_idが0だと保存できないこと' do
+    end
+
+    it 'phoneに-（ハイフン）が入っていると保存できないこと' do
+    end
+
+    it 'phoneが12桁以上だと保存できないこと' do
+    end
+
+    it 'userが紐づいていないと保存できないこと' do
+    end
+
+    it 'itemが紐づいていないと保存できないこと' do
+    end
+  end
 end

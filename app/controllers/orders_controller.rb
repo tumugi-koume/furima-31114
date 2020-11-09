9285fbd853b5a70c0109328cf0929cd59702b9bb
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-09 18:23:41 +0900

    売却済商品の購入画面に遷移するとトップページに遷移するよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
   end
 
   def remove_to_index
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
 end
